aa3305c09fb9554e6baf67ac11dbb4962ed65adc
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-16 22:32:34 +0530

    Verifying intent one

--- app/src/main/java/com/sagar/commonintents/MainActivity.java
+++ app/src/main/java/com/sagar/commonintents/MainActivity.java
@@ -1,16 +1,39 @@
 package com.sagar.commonintents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
     }
+
+
+    public void openWebSite(View view) {
+        Uri webpage = Uri.parse("https://developer.android.com/guide/components/intents-common");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+
+    public void showLocationInMap(View view) {
+        
+    }
+
+
+    public void shareTextMessage(View view) {
+    }
+
+
+    // END
 }
