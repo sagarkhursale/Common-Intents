69763b4f8c9e60d45f8fb41b49e86c2effe38b88
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-08-17 00:24:13 +0530

    Share intent verified..!

--- app/src/main/java/com/sagar/commonintents/MainActivity.java
+++ app/src/main/java/com/sagar/commonintents/MainActivity.java
@@ -2,6 +2,7 @@ package com.sagar.commonintents;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,11 +28,32 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void showLocationInMap(View view) {
-        
+        String addressString = "1600 Amphitheatre Parkway, CA";
+
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo")
+                .path("0,0")
+                .query(addressString);
+        Uri addressUri = builder.build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 
     public void shareTextMessage(View view) {
+        String mimeType = "text/plain";
+        String title = "Learning how to share?";
+        String textToshare = "Hello there!";
+
+        ShareCompat.IntentBuilder.from(this)
+                .setChooserTitle(title)
+                .setType(mimeType)
+                .setText(textToshare)
+                .startChooser();
     }
 
 
